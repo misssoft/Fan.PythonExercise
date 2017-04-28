a10a3ca8a827b093f91462a0a6d11ef7cd58b680
Fan Yang <fan.yang.turner@live.co.uk>
2017-04-29 00:07:15 +0100

    Update setup and manifest

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from distutils.core import setup
 
 setup(
     name='assessment',
-    versiion='0.1',
+    version='0.1',
+    author='dou',
+    author_email='dou@gmail.com',
+    url='dou.asssessment.com',
     packages=['assessment'],
     package_dir={'assessment':'src/assessment'},
-    package_data={'assessmeng':['data/*.csv']},
+    package_data={'assessment': ['data/*.csv']},
     )
-    
